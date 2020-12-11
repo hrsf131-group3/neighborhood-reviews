a1dc5fa77165c6580d329a4dface98b90c2f6282
Amelia Yeoh <ameyjm@gmail.com>
2020-12-11 15:40:05 -0800

    Updated reviews schema and data to use collection types

--- data/generateReviews.js
+++ data/generateReviews.js
@@ -4,8 +4,9 @@ const faker = require('faker');
 const cliProgress = require('cli-progress');
 
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+const bar2 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-const boolean = ['true', 'false'];
+const boolean = [true, false];
 const userTypes = ['Resident', 'Visitor', 'Resident', 'Resident', 'Resident'];
 
 const randomDate = function () {
@@ -16,18 +17,18 @@ const randomDate = function () {
 
 const textGenerator = new LorenIpsum({
   sentencesPerParagraph: {
-    max: 4,
-    min: 2,
+    max: 2,
+    min: 1,
   },
   wordsPerSentence: {
-    max: 16,
+    max: 10,
     min: 4,
   },
 });
 
-// const writeReviewsSQL = fs.createWriteStream('reviews.csv');
-// writeReviewsSQL.write('userId,neighborhoodId,reviewDate,reviewText,'
-//     + 'likes,community,commute\n', 'utf8');
+const writeReviewsSQL = fs.createWriteStream('reviews.csv');
+writeReviewsSQL.write('userId,neighborhoodId,reviewDate,reviewText,'
+    + 'likes,community,commute\n', 'utf8');
 
 const writeReviewsNoSQL = fs.createWriteStream('reviewsByNeighborhood.csv');
 writeReviewsNoSQL.write('listingId,neighborhoodId,username,userType,dogOwner,parent,'
@@ -36,7 +37,7 @@ writeReviewsNoSQL.write('listingId,neighborhoodId,username,userType,dogOwner,par
 function generateReviews(db, numReviews, numUsers, numNeighborhoods, numListings, writer, encoding, callback) {
   let i = numReviews;
   let idx = 0;
-  bar1.start(numNeighborhoods, 0);
+  bar1.start(numReviews, 0);
 
   function write() {
     let ok = true;
@@ -76,13 +77,76 @@ function generateReviews(db, numReviews, numUsers, numNeighborhoods, numListings
   write();
 }
 
+const writeNewReviews = fs.createWriteStream('newreviews.csv');
+writeNewReviews.write('neighborhoodId|listingId|reviews\n', 'utf8');
+
+// create list of reviews
+const generateReviewSet = function (noUsers, noReviews) {
+  const reviews = [];
+  for (let i = 1; i <= noReviews; i += 1) {
+    const username = `username:${faker.name.findName()}`;
+    const userType = `userType:${userTypes[i % 5]}`;
+    const dogOwner = `dogOwner:${boolean[i % 2]}`;
+    const parent = `parent:${boolean[i % 2]}`;
+    const userId = `userId:${Math.floor(Math.random() * noUsers) + 1}`;
+    const reviewDate = `reviewDate:${randomDate()}`;
+    const reviewText = `reviewText:${textGenerator.generateParagraphs(1)}`;
+    const likes = `likes:${Math.floor(Math.random() * 150) + 1}`;
+    const community = `community:${boolean[i % 2]}`;
+    const commute = `commute:${boolean[i % 2]}`;
+    const review = `{${username},${userType},${dogOwner},${parent},${userId},${reviewDate},${reviewText},${likes},${community},${commute}}`;
+    reviews.push(review);
+  }
+  return `[${reviews.toString()}]`;
+};
+// console.log(generateReviewSet(1, 1));
+
+function generateNewReviews (numReviews, numUsers, numNeighborhoods, numListings, writer, encoding, callback) {
+  let i = numNeighborhoods;
+  let idx = 0;
+  bar2.start(numNeighborhoods, 0);
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      idx += 1;
+      if (idx % 100000 === 0) {
+        bar1.update(idx);
+      }
+      const neighborhoodId = idx;
+      const listingId = Math.floor(Math.random() * numListings) + 1;
+      const randomReviewNo = Math.floor(Math.random() * 15) + 1;
+      const reviews = generateReviewSet(numUsers, randomReviewNo + 2);
+      const data = `${neighborhoodId}|${listingId}|${reviews}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
 // generate 100M reviews with 10M users and 10M neighborhoods
 async function generateCSV() {
-  // await generateReviews('sql', 100000000, 10000000, 10000000, writeReviewsSQL, 'utf-8', () => {
-  //   writeReviewsSQL.end();
-  // });
+  await generateReviews('sql', 100000000, 10000000, 10000000, writeReviewsSQL, 'utf-8', () => {
+    writeReviewsSQL.end();
+  });
+  await generateReviews('nosql', 1000, 100, 100, 2000, writeReviewsNoSQL, 'utf-8', () => {
+    writeReviewsNoSQL.end();
+  });
   await generateReviews('nosql', 100000000, 10000000, 10000000, 200000000, writeReviewsNoSQL, 'utf-8', () => {
     writeReviewsNoSQL.end();
+  });
+  await generateNewReviews(100000000, 10000000, 10000000, 200000000, writeNewReviews, 'utf-8', () => {
+    writeNewReviews.end();
+  });
+  await generateNewReviews(100, 10, 10, 200, writeNewReviews, 'utf-8', () => {
+    writeNewReviews.end();
   });
 }
 
